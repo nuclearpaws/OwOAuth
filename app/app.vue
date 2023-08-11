07e85833b1dd69827a1828bf5556f0e39cbfe411
NuclearPaws <nuclearpaws@gmail.com>
2023-08-11 23:04:40 +0100

    Added theme support for tailwind.

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,25 @@
+<script setup>
+const colorMode = useColorMode();
+</script>
+
 <template>
-    <NuxtLayout>
-        <NuxtPage />
-    </NuxtLayout>
+    <div>
+        <select
+            v-model="colorMode.preference"
+            class="border w-24 h-8 dark:bg-gray-900 dark:text-white dark:border-gray-700"
+        >
+            <option value="system">System</option>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+        </select>
+        <NuxtLayout>
+            <NuxtPage class="mx-auto p-4" />
+        </NuxtLayout>
+    </div>
 </template>
 
-<script setup></script>
+<style lang="postcss">
+body {
+    @apply min-h-screen bg-white dark:bg-gray-800 dark:text-gray-200;
+}
+</style>
